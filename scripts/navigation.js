e23d82e4b6afdeaa3aa5321050c919f2775cd5af
AlessandroRomanelli <aleromrod@gmail.com>
2017-01-22 02:39:52 +0100

    Main Title animation time-based added and restyles

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,16 +1,26 @@
 // Show a button that always floats on top of the page and shows the navigationMenu
 // when clicked upon
 
+
+
 $("#navigationFloater").mouseenter(function(){
+  var y = $(window).scrollTop();
+  $(window).scrollTop(y-10);
   $("#top").addClass("hoverTransform");
 });
 
 $("#top").mouseleave(function(){
+  var y = $(window).scrollTop();
+  $(window).scrollTop(y+40);
   $(this).removeClass("hoverTransform");
+  $(this).removeClass("clickTransform");
+  $("#navigationFloater").text("▼ Show Navigation Bar ▼")
 });
 
 $("#navigationFloater").click(function(){
   if ($("#top").attr("class") == "hoverTransform") {
+    var y = $(window).scrollTop();
+    $(window).scrollTop(y-40);
     $("#top").addClass("clickTransform");
     $("#top").removeClass("hoverTransform");
     $(this).text("▲ Hide Navigation Bar ▲");
@@ -19,6 +29,8 @@ $("#navigationFloater").click(function(){
 
 $("#navigationFloater").click(function(){
 if ($("#top").attr("class") == "clickTransform hoverTransform") {
+      var y = $(window).scrollTop();
+      $(window).scrollTop(y+40);
       $("#top").removeClass("clickTransform");
       $(this).text("▼ Show Navigation Bar ▼")
 }});
